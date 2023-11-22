6e386fb4bc68df13b7426ffbb9d7a63a371fc459
Jonathan-Agreda <flavioagreda1990@gmail.com>
2023-11-21 19:06:43 -0500

    PREVIO AÑADIR TELEGRAM

--- PROYECTO_TRACKIOT/PROYECTO_TRACKIOT.ino
+++ PROYECTO_TRACKIOT/PROYECTO_TRACKIOT.ino
@@ -4,7 +4,6 @@
 void setup() {
   setupWifi();
   setupGps();
-
 }
 
 void loop() {
--- PROYECTO_TRACKIOT/vargps.h
+++ PROYECTO_TRACKIOT/vargps.h
@@ -6,7 +6,7 @@ String apiDevicesGps = "http://135.148.148.73:3000/api/devicesgps/1";
 String apiDevicesPanico = "http://135.148.148.73:3000/api/devicespanico/1";
 String bus = "Piloto"; 
 int periodoGpsPut = 20000; unsigned long TiempoAhoraGpsPut = 0;
-int periodoPanico = 40000; unsigned long TiempoAhoraPanico = 0;
+int periodoPanico = 5000; unsigned long TiempoAhoraPanico = 0;
 
 String fechaGps = "dd/mm/aaaa";
 String horaGps = "hh:mm:ss" ;
