d35bc33c823529508520cc943438aadf173acf32
Jonathan-Agreda <flavioagreda1990@gmail.com>
2023-12-06 07:49:12 -0500

    DEPLOY

--- PROYECTO_TRACKIOT/vargps.h
+++ PROYECTO_TRACKIOT/vargps.h
@@ -5,7 +5,7 @@ String apiPanico = "http://135.148.148.73:3000/api/panico";
 String apiDevicesGps = "http://135.148.148.73:3000/api/devicesgps/1";
 String apiDevicesPanico = "http://135.148.148.73:3000/api/devicespanico/1";
 String bus = "abcd-1234"; 
-int periodoGpsPut = 20000; unsigned long TiempoAhoraGpsPut = 0;
+int periodoGpsPut = 3000; unsigned long TiempoAhoraGpsPut = 0;
 int periodoPanico = 5000; unsigned long TiempoAhoraPanico = 0;
 
 String fechaGps = "dd/mm/aaaa";
